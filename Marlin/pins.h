ee84e73a486f2c1095fd0702de97141fc03edd89
f1oat <frible@teaser.fr>
2015-07-14 22:43:33 +0200

    ZMIN is PROBE pin

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -1360,7 +1360,7 @@
 
 #define Z_STEP_PIN 37
 #define Z_DIR_PIN 39
-#define Z_MIN_PIN 30
+#define Z_MIN_PIN 13 // 30 : use proximity sensor instead of ZMIN switch
 #define Z_MAX_PIN 32
 #define Z_ENABLE_PIN 35
 
@@ -1387,7 +1387,7 @@
 #define SDPOWER            -1
 #define SDSS               53
 #define LED_PIN            -1
-#define PROBE_PIN			13	// Proximity sensor for bed auto leveling
+#define PROBE_PIN			-1	// Proximity sensor for bed auto leveling
 #define FAN_PIN            7
 #define PS_ON_PIN          12
 #define KILL_PIN           -1
