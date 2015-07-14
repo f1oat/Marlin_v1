10d0396348c98d0de8e18ccafaad711eb45cbf31
f1oat <frible@teaser.fr>
2015-07-14 18:55:41 +0200

    Power on machine at boot

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -102,7 +102,7 @@
 #define POWER_SUPPLY 2
 
 // Define this to have the electronics keep the powersupply off on startup. If you don't know what this is leave it.
-#define PS_DEFAULT_OFF
+//#define PS_DEFAULT_OFF
 
 //===========================================================================
 //=============================Thermal Settings  ============================
